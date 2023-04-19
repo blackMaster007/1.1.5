a350cae4b1f5ba82ae3efa64f4fcbb749c522233
blackMaster007 <vturchkov@yandex.ru>
2023-04-19 10:58:06 +0300

    changes by comments changed: Main, UserServiceImpl

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,7 +3,6 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-
 import java.util.ArrayList;
 import java.util.List;
 
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -4,7 +4,6 @@ package jm.task.core.jdbc.service;
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
-
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
