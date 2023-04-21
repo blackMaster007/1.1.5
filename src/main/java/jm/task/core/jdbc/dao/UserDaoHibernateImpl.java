33b84fcf883d8b8fe72319e9ab7bbd907c1e3411
blackMaster007 <vturchkov@yandex.ru>
2023-04-21 13:57:25 +0300

    criteria were deleted

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -5,9 +5,6 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import javax.persistence.Query;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import java.util.List;
 
 import static jm.task.core.jdbc.util.Util.getSessionFactory;
@@ -35,16 +32,10 @@ public class UserDaoHibernateImpl implements UserDao {
             query.executeUpdate();
             transaction.commit();
         } catch (Exception e) {
-            System.out.println("Ошибка в методе createUsersTable");
-            e.printStackTrace();
-            try {
-                if (transaction != null) {
-                    transaction.rollback();
-                }
-            } catch (Exception e1) {
-                System.out.println("Ошибка при откате транзакции");
-                e1.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
             }
+            e.printStackTrace();
         }
     }
 
@@ -91,13 +82,13 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public List<User> getAllUsers() {
+        Transaction transaction;
+        List<User> list;
         try (Session session = getSessionFactory().openSession()) {
-            CriteriaBuilder cb = session.getCriteriaBuilder();
-            CriteriaQuery cq = cb.createQuery(User.class);
-            Root<User> root = cq.from(User.class);
-            cq.select(root);
-            Query query = session.createQuery(cq);
-            return query.getResultList();
+            transaction = session.beginTransaction();
+            list = session.createQuery("FROM User").getResultList();
+            transaction.commit();
+            return list;
         }
     }
 
@@ -106,7 +97,7 @@ public class UserDaoHibernateImpl implements UserDao {
         Transaction transaction = null;
         try (Session session = getSessionFactory().openSession()) {
             session.beginTransaction();
-            session.createSQLQuery("DELETE FROM users").executeUpdate();
+            session.createQuery("DELETE FROM User").executeUpdate();
             session.getTransaction().commit();
         } catch (Exception e) {
             if (transaction != null) {
